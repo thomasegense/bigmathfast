f1128c3117ee564cb19ba650c2dbe7158a091fba
teg <teg@kb.dk>
2020-08-27 14:32:06 +0200

    Javadoc on BigMathFast util class.

--- src/main/java/dk/teg/bigmathfast/BigMathFast.java
+++ src/main/java/dk/teg/bigmathfast/BigMathFast.java
@@ -4,14 +4,79 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 
 import ar.alpertron.ecm.Ecm;
+import dk.teg.bigmathfast.euler.EulerTotient;
+import dk.teg.bigmathfast.primes.PollardRho;
 
 public class BigMathFast {
 
-    
-    
-    public ArrayList<BigInteger> factorizeEmc(BigInteger b){        
-        return Ecm.factor(b);
+    /*
+     * Just an example to run the factorization from a command line.
+     * Example :
+     *  java -cp bigmathfast-1.0-jar-with-dependencies.jar dk.teg.bigmathfast.BigMathFast 93035149443954345347665179408833277091909532522394543659489519897196854705698057
+     *  This 70 digits will be factorized in 25 seconds. This is worst case for a 70 digits number. 
+     * 
+     */
+    public static void main(String[] args) {
+        BigInteger b= new BigInteger(args[0]);
+        long start = System.currentTimeMillis();
+        ArrayList<BigInteger> factors = Ecm.factor(b);
+        System.out.println("Factotorization time in millis:"+(System.currentTimeMillis()-start));
+        System.out.println(factors);          
     }
     
     
+    /**
+     * This method will factorize an integer into prime factors. 
+     * 
+     * 
+     * If the number has less than 22 digits, the Pollardh algorithm will be used.
+     * If the number has 22 digits or more, the ECM/Siqs  algorithm will be used.
+     * 
+     * A number with 70 digits will be factorized in 30 seconds in worst case.
+     *    
+     * @see https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm
+     * @see https://www.alpertron.com.ar/ECM.HTM
+     *   
+     * @param b The BigInteger to be factorized
+     * @return ArrayList<BigInteger> with the prime factors in sorted order. 
+     *  
+     */    
+    public static ArrayList<BigInteger> factorize(BigInteger b){        
+        if (b.toString().length() <22) {
+            return PollardRho.factor(b); 
+        }
+        else {
+          return Ecm.factor(b);                
+        }       
+    }
+                    
+    /**
+     * Calculate the Euler Totient (phi) for an number     
+     * Running time is dependant on the factorization of the input number
+     * @see https://en.wikipedia.org/wiki/Euler%27s_totient_function    
+     *   
+     * @param b The BigInteger to calculate the Euler Totient
+     * @return BigInteger The Euler Totient 
+     *  
+     */    
+    public static BigInteger eulerTotient(BigInteger b) {        
+        return EulerTotient.eulerTotient(b);
+    }
+        
+    
+    /**
+     * Calculate the Inverse Euler Totient (invphi) for an number
+     * Use algorithm described by Hansraj Gupta and is the fastests known.
+     * 
+     * @see http://www.new.dli.ernet.in/rawdataupload/upload/insa/INSA_2/20005a81_22.pdf      
+     * @see https://en.wikipedia.org/wiki/Euler%27s_totient_function          
+     *   
+     * @param b The BigInteger to calculate the Euler Totient
+     * @return ArrayList<BigInteger> All numbers have Euler Totient equal to b 
+     *  
+     */
+    public static ArrayList<BigInteger> inverseEulerTotient(BigInteger b) {        
+        return EulerTotient.inverseEulerTotient(b);
+    }
+        
 }
