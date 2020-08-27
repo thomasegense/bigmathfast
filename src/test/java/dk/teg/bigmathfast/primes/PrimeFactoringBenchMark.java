b34ee2944b2a1f8941dc786cc76ee7ceae81afec
teg <teg@kb.dk>
2020-08-27 14:32:37 +0200

    refactoring

--- src/test/java/dk/teg/bigmathfast/primes/PrimeFactoringBenchMark.java
+++ src/test/java/dk/teg/bigmathfast/primes/PrimeFactoringBenchMark.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
-import dk.thomasegense.bigmathfast.primes.ecm.Ecm;
+import ar.alpertron.ecm.Ecm;
+
 
 public class PrimeFactoringBenchMark {
 
@@ -17,18 +18,26 @@ public class PrimeFactoringBenchMark {
 
         long timeRho=0;
         long timeEcm=0;
-        for (int i =1 ;i<100;i++) {                     
-            String number = generateNumber(24);
-                        
+        for (int i =1 ;i<10000;i++) {                     
+            String number = generateNumber(70);
+            
+            //number="90105590165517928753889708611168689116043190507997";
+            System.out.println(number);
             long start = System.currentTimeMillis();
-            ArrayList<BigInteger> factorRho = PollardRho.factor(new BigInteger(number));
+            ArrayList<BigInteger> factorRho = Ecm.factor(new BigInteger(number));
             timeRho += (System.currentTimeMillis()-start);
                                 
+            
             start = System.currentTimeMillis();
             ArrayList<BigInteger> factorEcm= Ecm.factor(new BigInteger(number));
             timeEcm += (System.currentTimeMillis()-start);
             assertEquals(factorRho, factorEcm);
-            System.out.println(timeRho +":"+timeEcm +" number:"+number);                         
+            System.out.println(timeRho +":"+timeEcm +" number:"+number);
+            
+
+            
+            //System.out.println(timeEcm +" number:"+number);
+                                     
         }       
     }
 
--- src/test/java/dk/teg/bigmathfast/primes/Test.java
+++ src/test/java/dk/teg/bigmathfast/primes/Test.java
@@ -2,14 +2,16 @@ package dk.teg.bigmathfast.primes;
 
 import java.math.BigInteger;
 
-import dk.thomasegense.bigmathfast.primes.ecm.*;
+import ar.alpertron.ecm.Ecm;
+
+
 
 public class Test {
 
     public static void main(String[] args) {
        
         
-  System.out.println(ecm.factor(new BigInteger("30323232323232323231243244272")));
+  System.out.println(Ecm.factor(new BigInteger("30323232323232323231243244272")));
         
         
         
